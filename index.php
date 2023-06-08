55c30ad3d83f75798119a71227ff809d7faf8889
rohgibong <rohgibong@gmail.com>
2023-06-08 14:38:42 +0900

    Last Commit

--- index.php
+++ index.php
@@ -45,7 +45,6 @@
       </div>
       <div id="searchDiv">
         <input type="text" name="searchInput" id="searchInput" onkeydown="if(event.keyCode==13) search()">  
-        <!-- placeholder="찾고 싶은 상품을 검색해보세요!" -->
         <button type="button" id="searchBtn" onClick="search();">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 20">
             <path d="M21.71 20.29l-5.01-5.01C17.54 13.68 18 11.91 18 10c0-4.41-3.59-8-8-8S2 5.59 2 10s3.59 8 8 8c1.91 0 3.68-.46 5.28-1.3l5.01 5.01c.39.39 1.02.39 1.41 0l1.41-1.41c.38-.38.39-1.01 0-1.4zM4 10c0-3.31 2.69-6 6-6s6 2.69 6 6-2.69 6-6 6-6-2.69-6-6z"/>
